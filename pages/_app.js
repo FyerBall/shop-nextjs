fdf156f5af5fb15b6fd227d4d735255827d0eacd
Meshari <alh3rbi@gmail.com>
2021-08-12 10:24:32 -0600

    added nprogress css

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import PageLayout from "../layout/PageLayout/PageLayout";
 import "../styles/globals.css";
 import { useState } from "react";
-import Head from "next/head";
 import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
 NProgress.configure({ showSpinner: false });
 
 import Router from "next/router";
@@ -22,15 +23,6 @@ function MyApp({ Component, pageProps }) {
   });
   return (
     <>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
-          integrity="sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=="
-          crossOrigin="anonymous"
-          referrerpolicy="no-referrer"
-        />
-      </Head>
       {/* ref. for responsive-ness */}
       <div className="bg-red-500 h-2 sm:bg-green-500 md:bg-blue-500 lg:bg-purple-500 xl:bg-yellow-500 2xl:bg-pink-500"></div>
       {isLoading && (
